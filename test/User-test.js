88eb512e0a5d9b287bb9e7ccd2bb49b765e27bcd
Angela Battillo <angela.battillo@gmail.com>
2021-03-28 13:57:42 -0600

    Start first it block template

--- test/User-test.js
+++ test/User-test.js
@@ -2,17 +2,19 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const User = require('../src/UserClass');
-const Sleep = require('../src/SleepClass');
-const Activity = require('../src/ActivityClass');
-const Hydration = require('../src/HydrationClass');
+const testUserData = require('../data/test-user-data.js');
 
 describe ('User', () => {
   let user1, user2;
 
   beforeEach(() => {
-    user1 = new User(testUserData[0].id, testUserData[0].name, testUserData[0].address, testUserData[0].email, testUserData[0].strideLength, testUserData[0].dailyStepGoal, testUserData[0].friends);
-    user2 = new User(testUserData[1].id, testUserData[1].name, testUserData[1].address, testUserData[1].email, testUserData[1].strideLength, testUserData[1].dailyStepGoal, testUserData[1].friends);
-
+    user1 = new User(testUserData[0]);
+    user2 = new User(testUserData[1]);
   });
+
+  it('should be a function', () => {
+    expect(User).to.be.a('function');
+  });
+
 
 });
