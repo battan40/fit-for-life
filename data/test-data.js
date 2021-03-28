cc0e65a3dc34ec2e3c1054bbe2b04d82b9a309e7
Angela Battillo <angela.battillo@gmail.com>
2021-03-28 11:02:57 -0600

    Start skeleton of data file

---
+++ data/test-data.js
@@ -0,0 +1,43 @@
+const userTestingData = [
+  {
+    "id": 1,
+    "name": "Lilith Black Moon",
+    "address": "55555 Magic Castle Sky, Erdmanport VT 19901-1697",
+    "email": "Lilith.Hayes1@hotmail.com",
+    "strideLength": 4.3,
+    "dailyStepGoal": 10000,
+    "friends": [
+      16,
+      4,
+      8
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Inanna Ishtar",
+    "address": "300000, Sumerian Temple Way, Ciceroland NE 07273",
+    "email": "Inanna.Bechtelar11@gmail.com",
+    "strideLength": 4.5,
+    "dailyStepGoal": 5000,
+    "friends": [
+      9,
+      18,
+      24,
+      19
+    ]
+  },
+  {
+    "id": 3,
+    "name": "Sarasvati Devi",
+    "address": "77777 White Lotus Park, East Oscarstad MN 85126-5660",
+    "email": "RiverGoddess.Tromp@yahoo.com",
+    "strideLength": 4.4,
+    "dailyStepGoal": 5000,
+    "friends": [
+      19,
+      11,
+      42,
+      33
+    ]
+  },
+  
