ae303849815f30b231f6759cb5b9eb4eff5114b7
Angela Battillo <angela.battillo@gmail.com>
2021-03-28 11:52:10 -0600

    Separate out testing data

---
+++ data/test-user-data.js
@@ -0,0 +1,71 @@
+const testUserData = [
+  {
+    "id": 1,
+    "name": "Lilith Black Moon",
+    "address": "55555 Magic Castle Sky, Erdmanport VT 19901-1697",
+    "email": "Lilith.Hayes1@hotmail.com",
+    "strideLength": 4.3,
+    "dailyStepGoal": 10000,
+    "friends": [
+      16,
+      4,
+      8
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Inanna Ishtar",
+    "address": "300000, Sumerian Temple Way, Ciceroland NE 07273",
+    "email": "Inanna.Bechtelar11@gmail.com",
+    "strideLength": 4.5,
+    "dailyStepGoal": 5000,
+    "friends": [
+      9,
+      18,
+      24,
+      19
+    ]
+  },
+  {
+    "id": 3,
+    "name": "Sarasvati Devi",
+    "address": "77777 White Lotus Park, East Oscarstad MN 85126-5660",
+    "email": "RiverGoddess.Tromp@yahoo.com",
+    "strideLength": 4.4,
+    "dailyStepGoal": 5000,
+    "friends": [
+      19,
+      11,
+      42,
+      33
+    ]
+  },
+  {
+    "id": 4,
+    "name": "Laksmi Devi",
+    "address": "887755 Milky Ocean Drive, Turnermouth OR 23720-3230",
+    "email": "MotherOfTheWorld_Pollich@hotmail.com",
+    "strideLength": 3.1,
+    "dailyStepGoal": 4000,
+    "friends": [
+      48,
+      7,
+      44,
+      8
+    ]
+  },
+  {
+    "id": 5,
+    "name": "Diana Hunt",
+    "address": "3399 Forest Wilds Walk, Jordaneside WA 55023-6523",
+    "email": "Huntress_ofTheFold@gmail.com",
+    "strideLength": 3.1,
+    "dailyStepGoal": 8000,
+    "friends": [
+      13,
+      44,
+      49,
+      33,
+      10
+    ]
+  };
