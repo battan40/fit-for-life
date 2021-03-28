2461e586d228433ca5a3ba8016e63f2cf77be4f1
Angela Battillo <angela.battillo@gmail.com>
2021-03-28 11:54:59 -0600

    Rebuild test data files

---
+++ test/User-test.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+// const User = require('../test-user-data');
+
+
+describe ('User', () => {
+  let user1, user2, user3, activity, sleep, hydration;
+
+  beforeEach(() => {
+    user1 = new User()
+  });
+
+});
