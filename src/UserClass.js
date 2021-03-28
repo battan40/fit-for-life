0dd56ac986c169d177ee152c55bab4b841ba8bcf
Angela Battillo <angela.battillo@gmail.com>
2021-03-28 13:55:12 -0600

    Deconstruct class object for testing

--- src/UserClass.js
+++ src/UserClass.js
@@ -1,7 +1,13 @@
 class User {
-  constructor(
+  constructor({ id, name, address, email, strideLength, dailyStepGoal }) {
+    this.id = id;
+    this.name = name;
+    this.address = address;
+    this.email = email;
+    this.strideLength = strideLength;
+    this.dailyStepGoal = dailyStepGoal;
+  }
 
-  )
 };
 
 if (typeof module !== 'undefined') {
