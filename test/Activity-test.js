1d30ba1f6852d11414f6aeb2d82b987da2a4d589
Angela Battillo <angela.battillo@gmail.com>
2021-03-28 22:11:32 -0600

    Build in basic template for test

---
+++ test/Activity-test.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const User = require('../src/UserClass');
+const Activity = require('../src/ActivityClass');
+const testUserData = require('../data/test-user-data.js');
+const testActivityData = require('../data/test-activity-data.js');
+const activityData = testActivityData.activityData;
+
+describe ('Activity', () => {
+  let user1, user2, activity;
+
+  beforeEach(() => {
+    user1 = new User(testUserData[0]);
+    user2 = new User(testUserData[1]);
+    activity = new Activity(testActivityData, user1);
+  });
+
+});
