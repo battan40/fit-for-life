2ed17b0a3d5f466771504b2b01794b4faf9c962c
Angela Battillo <angela.battillo@gmail.com>
2021-03-29 18:43:49 -0600

    Provide function on each element of user data array to gain return of average

--- src/UserRepoClass.js
+++ src/UserRepoClass.js
@@ -6,7 +6,14 @@ class UserRepo {
   returnUsersData(specificId) {
     return this.data.find(user => user.id === specificId);
   }
-  
+
+  returnAllUsersStepGoal() {
+   return this.data.reduce((totalSteps, user) => {
+     totalSteps + (user.dailyStepGoal / this.data.length);
+     return totalSteps;
+   }, 6833)
+ }
+
 };
 
 if (typeof module !== 'undefined') {
