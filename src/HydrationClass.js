001a7bfbbc94d56dd6608fbc5790da3bf2ee5209
Angela Battillo <angela.battillo@gmail.com>
2021-04-01 08:27:10 -0600

    Rebuild class structure to include methods and simplify

--- src/HydrationClass.js
+++ src/HydrationClass.js
@@ -1,11 +1,29 @@
 class Hydration {
-  constructor({userID, date, numOunces}) {
-    this.userID = userID;
-    this.date = date;
-    this.numOunces = numOunces;
-  }
+  constructor(hydrationData) {
+    this.hydrationData = hydrationData
+    console.log(this.hydrationData)
+  };
 
-}
+  findUserById(userById) {
+    let uniqueUser = this.hydrationData.filter(user => {
+      console.log()
+      if (user.userID === userById)
+    return uniqueUser;
+  });
+};
+
+  calculateHydrationAllTime(userById) {
+    const userHydrationData = this.findUserById(userById).filter(oneUser => {
+      return oneUser.numOunces;
+    });
+    console.log(userHydrationData)
+    const allOuncesDrank = userHydrationData.reduce((beginningOunces, drinker) => {
+      return beginningOunce + drinker.numOunces;
+    }, 0);
+    console.log(allOuncesDrank)
+      return allOuncesDrank / this.userHydrationData.length;
+  };
+};
 
 if (typeof module !== 'undefined') {
   module.exports = Hydration;
--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -12,24 +12,22 @@ describe ('Hydration', () => {
     hydration2 = new Hydration(testHydrationData[1]);
   });
 
-  it('should be a function', function() {
+  it('should be a function', () => {
     expect(Hydration).to.be.a('function');
-  })
+  });
 
-  it('should have a user ID', function() {
-    expect(hydration1.userID).to.equal(1);
-    expect(hydration2.userID).to.equal(2);
-  })
+  it('should instantiate Hydration', () => {
+    expect(hydration1).to.be.an.instanceOf(Hydration);
+    expect(hydration2).to.be.an.instanceOf(Hydration);
+  });
 
-  it('should have a date', function() {
-    expect(hydration1.date).to.equal('2019/06/15');
-    expect(hydration2.date).to.equal('2019/06/15');
-  })
-
-  it('should recall number of Fluid Ounces drank', function() {
-    expect(hydration1.numOunces).to.equal(37);
-    expect(hydration2.numOunces).to.equal(75);
-  })
+  it('should be able to locate a single user by their unique id', () => {
+    expect(hydration1.findUserById(id)).to.deep.equal(hydration1.hydrationData.userID)
+  });
 
 
+  // it('should recall number of Fluid Ounces drank', function() {
+  //   expect(hydration1.numOunces).to.equal(37);
+  //   expect(hydration2.numOunces).to.equal(75);
+  // })
 });
