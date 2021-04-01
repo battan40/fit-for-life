463a1c0d3eb07a1da91563380e7573b3bb06ccff
Patrick Findley <patfindley@gmail.com>
2021-03-31 20:40:23 -0400

    Add hydration test

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -19,7 +19,16 @@ describe ('Hydration', () => {
   it('should have a user ID', function() {
     expect(hydration1.userID).to.equal(1);
     expect(hydration2.userID).to.equal(2);
+  })
 
+  it('should have a date', function() {
+    expect(hydration1.date).to.equal('2019/06/15');
+    expect(hydration2.date).to.equal('2019/06/15');
+  })
+
+  it('should recall number of Fluid Ounces drank', function() {
+    expect(hydration1.numOunces).to.equal(37);
+    expect(hydration2.numOunces).to.equal(75);
   })
 
 
