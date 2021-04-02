9a2728e6ea599acfc2456b921cfdbd9a98751210
Patrick Findley <patfindley@gmail.com>
2021-04-01 20:59:39 -0400

    Update testing

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -19,15 +19,15 @@ describe ('Hydration', () => {
   });
 
   it('should be able to locate a single users data by their unique id', () => {
-    console.log(hydration.findUserById(2), '<<<<<<')
     expect(hydration.findUserById(1)).to.deep.equal([testHydrationData[0], testHydrationData[5], testHydrationData[10], testHydrationData[15], testHydrationData[20], testHydrationData[25], testHydrationData[30], testHydrationData[35]]);
-    expect(hydration.findUserById(2)).to.deep.equal([testHydrationData[1], testHydrationData[6]], testHydrationData[11], testHydrationData[16], testHydrationData[21], testHydrationData[26], testHydrationData[31], testHydrationData[36]);
+    expect(hydration.findUserById(2)).to.deep.equal([testHydrationData[1], testHydrationData[6], testHydrationData[11], testHydrationData[16], testHydrationData[21], testHydrationData[26], testHydrationData[31], testHydrationData[36]]);
   });
 
-  it.skip('should recall average of all time Fluid Ounces drank', function() {
+  it('should recall average of all time Fluid Ounces drank', function() {
+    console.log(hydration.allOuncesDrank, '<<<<allozdrank')
     expect(hydration.calculateHydrationAllTime).to.be.a('function');
     expect(hydration.calculateHydrationAllTime(1)).to.equal(142);
-    expect(hydration.calculateHydrationAllTime(2)).to.equal(83);
+    expect(hydration.calculateHydrationAllTime(2)).to.equal(80.57);
   })
 
   it('should have method to recall fluid ounces drank from single date given', function() {
