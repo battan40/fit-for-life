87b9e2aa7b5bfa53cefdde2f9a04cb99bc4b1644
Patrick Findley <patfindley@gmail.com>
2021-04-02 22:52:28 -0400

    Update getMainUser function

--- src/scripts.js
+++ src/scripts.js
@@ -28,6 +28,7 @@ function getMainUser() {
   displayUser.innerText = currentUser.name;
   userStepGoal.innerText = ` Step Goal: ${currentUser.dailyStepGoal}`;
   stridelength.innerText = ` Stride length: ${currentUser.strideLength}`;
+  emailDisplay.innerText = `${currentUser.email}`;
 
 
 };
