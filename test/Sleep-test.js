44ce5e8bed3df29f803e91bb1e9719f174fd53b6
Angela Battillo <angela.battillo@gmail.com>
2021-04-02 20:02:28 -0600

    Add test skeleton

---
+++ test/Sleep-test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const expect = chai.expect;
+const User = require('../src/UserClass');
+const UserRepo = require('../src/UserRepoClass');
+const Sleep = require('../src/SleepClass');
+const testSleepData = require('../data/test-Sleep-data.js');
+const testUserData = require('../data/test-user-data.js');
+
+describe ('Hydration', () => {
+  let sleep, userRepo, user;
+
+  beforeEach(() => {
+    sleep = new Sleep(testSleepData);
+    userRepo = new UserRepo(testUserData);
+    user = new User(userRepo.returnUsersData(1))
+  });
+};
