a8c16a2b628a0c5a17c2db9ee54c74da18dc8055
Patrick Findley <patfindley@gmail.com>
2021-04-03 14:52:46 -0400

    Add findUserById method

--- src/ActivityClass.js
+++ src/ActivityClass.js
@@ -7,7 +7,14 @@ class Activity {
     this.activityData = activityData;
   }
 
+  findUserById(userId) {
+    return this.hydrationData.filter(user => {
+    return  user.userID === userId
+    })
 
+  milesWalkedOnDay(userID, date) {
+    const userStepData = this.findUserById(userID);
+  }
 
 };
 
@@ -17,3 +24,9 @@ class Activity {
 if (typeof module !== 'undefined') {
   module.exports = Activity;
 };
+
+
+//To determine the number of steps it will take you to walk a mile,
+//divide 5,280 by your step length.
+//To determine the number of strides it will take you to walk a mile,
+//divide 5,280 by your stride length.
