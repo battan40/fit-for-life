09f5b600bd1e7042833938cc7ba5c4b118dde975
Angela Battillo <angela.battillo@gmail.com>
2021-04-03 15:18:42 -0600

    Build out date and time methods for single user sleep class

--- src/SleepClass.js
+++ src/SleepClass.js
@@ -13,19 +13,56 @@ class Sleep {
     })
   };
 
-  dayAveHoursSleptOneUser(oneUser, oneDay) {
+  // enterNumberOnly(id) {
+  //
+  // }
+
+
+  hoursSleptOneUser(oneUser, oneDay) {
     const userSleepData = this.filterUserById(oneUser);
     const date = userSleepData.find(day => day.date === oneDay)
       return date.hoursSlept;
-  }
+  };
 
-  returnSleepQualityAllTime(oneId) {
+  sleepQualityOneUser(user, oneDate) {
+    const userSleepData = this.filterUserById(user);
+    const date = userSleepData.find(day => day.date === oneDate)
+      return date.sleepQuality;
+  };
+
+  aveUserHoursSleptAllTime(oneId) {
+    const userSleepData = this.filterUserById(oneId);
+    const allSleepQuality = userSleepData.reduce((startQuality, sleeper) => {
+      return startQuality + sleeper.hoursSlept;
+    }, 0);
+      return allSleepQuality / userSleepData.length;
+  };
+
+  returnAveUserSleepQualityAllTime(oneId) {
     const userSleepData = this.filterUserById(oneId);
     const allSleepQuality = userSleepData.reduce((startQuality, sleeper) => {
       return startQuality + sleeper.sleepQuality;
     }, 0);
       return allSleepQuality / userSleepData.length;
-  }
+  };
+
+  calculateWeeksHoursSlept(id, endDate) {
+      const userSleepData = this.filterUserById(id);
+      const begDate = moment(new Date(endDate)).subtract(6, 'days').format('YYYY/MM/DD');
+      const weekSleepHours = userSleepData.filter(week => {
+        return week.date >= begDate && week.date <= endDate
+      });
+      return weekSleepHours.map(weekHours => weekHours.hoursSlept);
+  };
+
+  calculateWeeklySleepQuality(id, endDate) {
+    const userSleepData = this.filterUserById(id);
+    const begDate = moment(new Date(endDate)).subtract(6, 'days').format('YYYY/MM/DD');
+    const weekSleepQuality = userSleepData.filter(week => {
+      return week.date >= begDate && week.date <= endDate
+    });
+    return weekSleepQuality.map(weekQuality => weekQuality.sleepQuality);
+  };
 };
 
 if (typeof module !== 'undefined') {
--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -34,13 +34,33 @@ describe ('Sleep', () => {
       { userID: 1, date: '2019/06/21', hoursSlept: 5.1, sleepQuality: 2.6 }]);
   });
 
+  // it('should only accept a number entry for hours slept', () => {
+  //   expect(sleep.enterNumberOnly(user.id)).to.equal(6.1)
+  // });
+
   it('should be able to record hours slept from a single user on a single date given', () => {
-    expect(sleep.dayAveHoursSleptOneUser(user.id, '2019/06/15')).to.equal(6.1);
+    expect(sleep.hoursSleptOneUser(user.id, '2019/06/15')).to.equal(6.1);
   });
 
-  it('should return average single user sleep quality of all time', () => {
-    expect(sleep.returnSleepQualityAllTime(1)).to.equal(3.385714285714286);
-    expect(sleep.returnSleepQualityAllTime(2)).to.equal(2.5142857142857147);
+  it('should be able to record hours slept from a single user on a single date given', () => {
+    expect(sleep.sleepQualityOneUser(user.id, '2019/06/15')).to.equal(2.2);
   });
 
+  it('should return average single user average sleep for any weeks date range', () => {
+    expect(sleep.aveUserHoursSleptAllTime(1)).to.equal(6.871428571428572);
+    expect(sleep.aveUserHoursSleptAllTime(2)).to.equal(7.328571428571428);
+  });
+
+  it('should return average single user average sleep quality any date range', () => {
+    expect(sleep.returnAveUserSleepQualityAllTime(1)).to.equal(3.385714285714286);
+    expect(sleep.returnAveUserSleepQualityAllTime(2)).to.equal(2.5142857142857147);
+  });
+
+  it('should return a list of hours slept each day per any week', () => {
+  expect(sleep.calculateWeeksHoursSlept(1, "2019/06/21")).to.deep.equal( [ 6.1, 7, 10.8, 5.4, 4.1, 9.6, 5.1 ]);
+  });
+
+  it('should return a list of sleep Quality numbers for each day per any week', () => {
+  expect(sleep.calculateWeeklySleepQuality(1, "2019/06/21")).to.deep.equal([ 2.2, 4.7, 4.7, 3, 3.6, 2.9, 2.6 ]);
+  });
 });
