1103533e65f1be59c05d2fbdb083efa86b16261d
Angela Battillo <angela.battillo@gmail.com>
2021-04-03 23:24:40 -0600

    Refactor for DRY js

--- src/SleepRepoClass.js
+++ src/SleepRepoClass.js
@@ -25,19 +25,18 @@ class SleepRepo {
     return sleepQualityValues / usersIdList.length;
   };
 
-  findQualityThreeSleep(endDate) {
-    const begDate = moment(new Date(endDate)).subtract(6, 'days').format('YYYY/MM/DD');
-    const weekSleepQuality = this.sleepData.filter(week => {
-      return week.date >= begDate && week.date <= endDate
-    });
-    const qualitySleepers = weekSleepQuality.reduce((allUsers, user) => {
-      if(!allUsers[user.userID]) {
-        allUsers[user.userID] = []
-      }
-      allUsers[user.userID].push(user.sleepQuality)
-        return allUsers;
+  locateQualitySleepers(weekSleepQuality) {
+    return weekSleepQuality.reduce((allUsers, user) => {
+    if(!allUsers[user.userID]) {
+      allUsers[user.userID] = []
+    }
+    allUsers[user.userID].push(user.sleepQuality)
+      return allUsers;
     }, {});
-    const averageSleepers = Object.keys(qualitySleepers).map(user => {
+  };
+
+  findAverageAllUserSleepQuality(qualitySleepers) {
+    return  Object.keys(qualitySleepers).map(user => {
       let allSleepTotals = qualitySleepers[user].reduce((total, num) => {
         return total + num;
       }, 0);
@@ -46,6 +45,15 @@ class SleepRepo {
         sleepQuality: Math.round((allSleepTotals / qualitySleepers[user].length) * 10) / 10
       }
     })
+  };
+
+  findQualityThreeSleep(endDate) {
+    const begDate = moment(new Date(endDate)).subtract(6, 'days').format('YYYY/MM/DD');
+    const weekSleepQuality = this.sleepData.filter(week => {
+      return week.date >= begDate && week.date <= endDate
+    });
+    const qualitySleepers = this.locateQualitySleepers(weekSleepQuality);
+    const averageSleepers = this.findAverageAllUserSleepQuality(qualitySleepers);
     return averageSleepers.filter(user => user.sleepQuality > 3.0)
   };
 };
