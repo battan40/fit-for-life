23918451a6e61d03cc6bfffdeadae98c9be63f50
Angela Battillo <angela.battillo@gmail.com>
2021-04-06 12:29:15 -0600

    Reduce space for dev empathy

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -27,77 +27,18 @@ describe ('Activity', () => {
 
   it('should be able to locate a single user\'s activity data by their unique id', () => {
 
-    expect(activity.findUserActivity(1)).to.deep.equal([
-  {
-    userID: 1,
-    date: '2019/06/15',
-    numSteps: 3577,
-    minutesActive: 140,
-    flightsOfStairs: 16
-  },
-  {
-    userID: 1,
-    date: '2019/06/16',
-    numSteps: 6637,
-    minutesActive: 175,
-    flightsOfStairs: 36
-  },
-  {
-    userID: 1,
-    date: '2019/06/17',
-    numSteps: 14329,
-    minutesActive: 168,
-    flightsOfStairs: 18
-  },
-  {
-    userID: 1,
-    date: '2019/06/18',
-    numSteps: 4419,
-    minutesActive: 165,
-    flightsOfStairs: 33
-  },
-  {
-    userID: 1,
-    date: '2019/06/19',
-    numSteps: 8429,
-    minutesActive: 275,
-    flightsOfStairs: 2
-  },
-  {
-    userID: 1,
-    date: '2019/06/20',
-    numSteps: 14478,
-    minutesActive: 140,
-    flightsOfStairs: 12
-  },
-  {
-    userID: 1,
-    date: '2019/06/21',
-    numSteps: 6760,
-    minutesActive: 135,
-    flightsOfStairs: 6
-  },
-  {
-    userID: 1,
-    date: '2019/06/22',
-    numSteps: 10289,
-    minutesActive: 119,
-    flightsOfStairs: 6
-  },
-  {
-    userID: 1,
-    date: '2019/06/23',
-    numSteps: 13928,
-    minutesActive: 218,
-    flightsOfStairs: 21
-  }
-]);
-  });
+     expect(activity.findUserActivity(1)).to.deep.equal([
+   {userID: 1, date: '2019/06/15', numSteps: 3577, minutesActive: 140, flightsOfStairs: 16},
+   {userID: 1, date: '2019/06/16', numSteps: 6637, minutesActive: 175, flightsOfStairs: 36},
+   {userID: 1, date: '2019/06/17', numSteps: 14329, minutesActive: 168, flightsOfStairs: 18},
+   {userID: 1, date: '2019/06/18', numSteps: 4419, minutesActive: 165, flightsOfStairs: 33},
+   {userID: 1, date: '2019/06/19', numSteps: 8429, minutesActive: 275, flightsOfStairs: 2},
+   {userID: 1, date: '2019/06/20', numSteps: 14478, minutesActive: 140, flightsOfStairs: 12},
+   {userID: 1, date: '2019/06/21', numSteps: 6760, minutesActive: 135, flightsOfStairs: 6},
+   {userID: 1, date: '2019/06/22', numSteps: 10289, minutesActive: 119, flightsOfStairs: 6},
+   {userID: 1, date: '2019/06/23', numSteps: 13928, minutesActive: 218, flightsOfStairs: 21}]);
+   });
 
-  it('should calculate miles walked on a single day for user', () => {
-    expect(activity.milesWalkedOnDay(1, "2019/06/16")).to.equal(5.405132575757576);
-    expect(activity.milesWalkedOnDay(2, "2019/06/18")).to.equal(3.973295454545455);
-  });
 
   it('should find how many minutes user was active for on date', () => {
     expect(activity.minutesActiveOnDay(1, "2019/06/16")).to.equal(175);
