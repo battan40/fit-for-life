e9846ac28aaff1883b2ce3ad659b408bcffb1b39
Angela Battillo <angela.battillo@gmail.com>
2021-04-06 12:56:21 -0600

    Remove white space

--- test/Activity-test.js
+++ test/Activity-test.js
@@ -44,14 +44,9 @@ describe ('Activity', () => {
      expect(activity.milesWalkedOnDay(user2, "2019/06/18")).to.equal(4);
    });
 
-   it.only('should count user steps on specific day', () => {
+   it('should count user steps on specific day', () => {
      expect(activity.stepsOnDate(1, "2019/06/22")).to.equal(10289);
      expect(activity.stepsOnDate(2, "2019/06/22")).to.equal(3605);
-   });
-
-   it('Should check if user achieved step goal on given date', () => {
-     expect(activity.AchieveGoal(user1, "2019/06/22")).to.equal(true);
-     expect(activity.AchieveGoal(user2, "2019/06/18")).to.equal(false);
    });
 
    it('should find how many minutes user was active for on date', () => {
@@ -64,23 +59,28 @@ describe ('Activity', () => {
     expect(activity.minutesActiveAverageOnWeek(2, "2019/06/21")).to.equal(24.857142857142858);
   });
 
+  it('Should check if user achieved step goal on given date', () => {
+    expect(activity.AchieveGoal(user1, "2019/06/22")).to.equal(true);
+    expect(activity.AchieveGoal(user2, "2019/06/18")).to.equal(false);
+  });
+
   it('Should check if user exceeded step goal in dates logged', () => {
-   expect(activity.exceededGoal(user1)).to.deep.equal([
- {userID: 1, date: '2019/06/17', numSteps: 14329, minutesActive: 168, flightsOfStairs: 18},
- {userID: 1, date: '2019/06/20', numSteps: 14478, minutesActive: 140, flightsOfStairs: 12},
- {userID: 1, date: '2019/06/22', numSteps: 10289, minutesActive: 119, flightsOfStairs: 6},
- {userID: 1, date: '2019/06/23', numSteps: 13928, minutesActive: 218, flightsOfStairs: 21}]);
-   expect(activity.exceededGoal(user2)).to.deep.equal([
- {userID: 2, date: '2019/06/17', numSteps: 13750, minutesActive: 65, flightsOfStairs: 4},
- {userID: 2, date: '2019/06/19', numSteps: 9858, minutesActive: 243, flightsOfStairs: 44},
- {userID: 2, date: '2019/06/20', numSteps: 8153, minutesActive: 74, flightsOfStairs: 10},
- {userID: 2, date: '2019/06/21', numSteps: 10225, minutesActive: 174, flightsOfStairs: 26}]);
+    expect(activity.exceededGoal(user1)).to.deep.equal([
+  {userID: 1, date: '2019/06/17', numSteps: 14329, minutesActive: 168, flightsOfStairs: 18},
+  {userID: 1, date: '2019/06/20', numSteps: 14478, minutesActive: 140, flightsOfStairs: 12},
+  {userID: 1, date: '2019/06/22', numSteps: 10289, minutesActive: 119, flightsOfStairs: 6},
+  {userID: 1, date: '2019/06/23', numSteps: 13928, minutesActive: 218, flightsOfStairs: 21}]);
+    expect(activity.exceededGoal(user2)).to.deep.equal([
+  {userID: 2, date: '2019/06/17', numSteps: 13750, minutesActive: 65, flightsOfStairs: 4},
+  {userID: 2, date: '2019/06/19', numSteps: 9858, minutesActive: 243, flightsOfStairs: 44},
+  {userID: 2, date: '2019/06/20', numSteps: 8153, minutesActive: 74, flightsOfStairs: 10},
+  {userID: 2, date: '2019/06/21', numSteps: 10225, minutesActive: 174, flightsOfStairs: 26}]);
  });
+
 
   it('should recall user\'s all time stair climb record', () => {
     expect(activity.allTimeStairClimb(1)).to.equal(`All time record is 36 flights of stairs, on 2019/06/16!`);
     expect(activity.allTimeStairClimb(2)).to.equal(`All time record is 44 flights of stairs, on 2019/06/19!`)
-
-  })
+  });
 
 });
