75068de0bb3f31ef299ebca401d0c513d1f9f81b
Angela Battillo <angela.battillo@gmail.com>
2021-04-07 18:25:15 -0600

    Remove white space

--- src/HydrationRepoClass.js
+++ src/HydrationRepoClass.js
@@ -1,21 +1,21 @@
 
 class HydrationRepo {
-  constructor(hydrationData){
+  constructor(hydrationData) {
     this.hydroData = hydrationData;
   }
 
   returnAverageAllHydration() {
     const allWaterDrank = [];
-    const allOunces= this.hydroData.reduce((totalOunces, user) => {
-    return totalOunces += user.numOunces;
+    const allOunces = this.hydroData.reduce((totalOunces, user) => {
+      return totalOunces += user.numOunces;
     }, 0);
     if (!this.hydroData.includes(this.hydroData.userID)) {
       allWaterDrank.push(this.hydroData.userID);
     }
     return allOunces / allWaterDrank.length;
   }
-};
+}
 
 if (typeof module !== 'undefined') {
   module.exports = HydrationRepo;
-};
+}
